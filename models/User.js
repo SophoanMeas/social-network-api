125791c468321512a11665ac0ee27c088626c1df
Sophoan Meas <sophoan.meas@hotmail.com>
2022-07-01 00:25:31 -0400

    add User model

---
+++ models/User.js
@@ -0,0 +1,45 @@
+const { Schema, model, Types } = require("mongoose");
+
+const UserSchema = new Schema(
+  {
+    username: {
+      type: String,
+      unique: true,
+      required: true,
+      trim: true,
+    },
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      match: [/.+@.+\..+/],
+    },
+    thoughts: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Thought",
+      },
+    ],
+    friends: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "User",
+      },
+    ],
+  },
+  {
+    toJson: {
+      virtuals: true,
+    },
+    id: false,
+  }
+);
+
+// virtual
+UserSchema.virtual('friendCount').get(()=>{
+    return this.friends.length
+})
+
+const User = model('User', UserSchema);
+
+model.exports = User;
