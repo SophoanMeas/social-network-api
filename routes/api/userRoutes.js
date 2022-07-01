23b5b6265d9fb118b056a0ebc83ccc1fed8e0677
Sophoan Meas <sophoan.meas@hotmail.com>
2022-07-01 01:52:32 -0400

    add api userRoutes.js

---
+++ routes/api/userRoutes.js
@@ -0,0 +1,32 @@
+const router = require('express').Router();
+
+const {
+  getAllUsers,
+  getUser,
+  createUser,
+  updateUser,
+  deleteUser,
+  createFriend,
+  deleteFriend,
+} = require('../../controllers/user-controller');
+
+// get all users; /api/users
+router
+    .route('/')
+    .get(getAllUsers)
+    .post(createUser);
+
+// get users by id /api/users/:userId
+router
+    .route('/:userid')
+    .get(getUser)
+    .put(updateUser)
+    .delete(deleteUser);
+
+// create new friend /api/users/:userId/friends/:friendId
+router
+    .route('/:userid/friends/:friendId')
+    .post(createFriend)
+    .delete(deleteFriend);
+
+module.exports = router;
