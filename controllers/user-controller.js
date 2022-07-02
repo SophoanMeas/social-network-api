fe61fc588a4287ebfcfd6434ed673b9e8d57b15b
Sophoan Meas <sophoan.meas@hotmail.com>
2022-07-02 02:47:17 -0400

    add user-controller

--- controllers/user-controller.js
+++ controllers/user-controller.js
@@ -1,4 +1,3 @@
-const { createSecureServer } = require('http2');
 const { User } = require('../models');
 
 const UserController = {
@@ -21,11 +20,97 @@ const UserController = {
       });
   },
 
-  // create user
+  // get a user by id
+  getUserById({ params }, res) {
+    User.findOne({ _id: params.id })
+      .populate({
+        path: 'Thoughts',
+        select: '-__v',
+      })
+      .populate({
+        path: 'Friends',
+        select: '-__v',
+      })
+      .select('-__v')
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => {
+        console.log(err);
+        res.sendStatus(404);
+      });
+  },
+
+  // add a user
   createUser({ body }, res) {
     User.create(body)
-    .then((dbUserData) => res.json(dbUserData))
-    .catch(err => res.sendStatus(400))
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.sendStatus(400));
+  },
+
+  // update a user
+  updateUser({ params, body }, res) {
+    User.findOneAndUpdate({ _id: params.id }, body, {
+      new: true,
+      runValidators: true,
+    })
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with tis id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.sendStatus(404).json(err));
+  },
+
+  // remove a user
+  deleteUser({ params }, res) {
+    User.findOneAndDelete({ _id: params.id })
+      .then((dbUserData) => res.json(dbUserData))
+      .catch((err) => res.sendStatus(404).json(err));
+  },
+
+  // add a new friend to a user's friend list
+  createFriend({ params }, res) {
+    User.findOneAndUpdate(
+      { _id: params.id },
+      { $push: { friends: params.id } },
+      { new: true, runValidators: true }
+    )
+      .populate({
+        path: 'friends',
+        select: '-__v',
+      })
+      .select('-__v')
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.sendStatus(404).json(err));
+  },
+
+  // delete a friend from a user's friend list
+  deleteFriend({ params }, res) {
+    User.findOneAndDelete(
+      { _id: params.id },
+      { $pull: { friend: params.id } },
+      { new: true }
+    )
+      .populate({
+        path: 'friends',
+        select: '-__v',
+      })
+      .select('-__v')
+      .then((dbUserData) => {
+        if (!dbUserData) {
+          res.status(404).json({ message: 'No user found with this id!' });
+          return;
+        }
+        res.json(dbUserData);
+      })
+      .catch((err) => res.sendStatus(404).json(err));
   },
 };
 
